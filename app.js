fefd37b6a219a8a31a881b7d326467e5d9946040
zcva827 <daniel.massey.2015@rhul.ac.uk>
2018-01-13 21:40:14 +0000

    Song selector moved and is highlightable

--- app.js
+++ app.js
@@ -1,65 +1,105 @@
-var json2 = "waiting";
-fetch('https://802eb561.ngrok.io/song/foo/popularity').then(function (value) {
-    json2 = value;
-    alert(json2);
-    app.$.change(value);
+var object = new Object();
+object["data"] = "test";
+var temp = fetch('http://localhost:8000/song/foo/popularity').then(function (value) {
+    object = value["data"];
+    console.log(value);
+    console.log(object);
 });
 
-Vue.component('song-item', {
-    props: ['song'],
-    template: '<li class="mdl-menu__item">{{ song.text }}</li>'
-})
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'SongBird',
-        allSongs: [
-            {id:0, text: 'first song'},
-            {id:1, text: 'second song'},
-            {id:2, text: 'third song'}
-        ],
-        json: json2
+        message: 'Hello Vue!',
+        songs: [
+            {id:0, text:"September - Earth, Wind & Fire", selected:true},
+            {id:1, text:"Uptown Funk - Bruno Mars", selected:false},
+            {id:2, text:"Hey Ya! - Outkast", selected:false}]
     },
     methods: {
-        change: function (value2) {
-            app.json = value2;
-            alert("called");
+        change: function (id) {
+            for (i = 0; i < songs.length; i++) {
+                this.songs[i].selected = false;
+            }
+            this.songs[id].selected = true;
+            set(id);
         }
     }
-});
-Vue.config.devtools = true
+})
 
 
+dates = ["02/17", "03/17", "04/17", "05/17", "06/17", "07/17", "08/17", "09/17", "10/17", "11/17", "12/17", "01/18"];
+songs = ["September - Earth, Wind & Fire", "Uptown Funk - Bruno Mars", "Hey Ya! - Outkast"];
+countries = ["UK", "USA", "Canada", "Australia"];
+uk0 = [24,35,54,57,46,36,35,38,40,44,42,46];
+us0 = [34,30,32,25,27,29,30,26,32,33,30,31];
+ca0 = [30,32,33,31,32,34,30,32,35,37,39,35];
+au0 = [24,25,28,30,28,26,29,28,30,32,31,32];
 
+uk1 = [45,40,44,41,42,38,39,36,40,41,42,38];
+us1 = [42,44,43,46,43,41,38,36,38,38,36,35];
+ca1 = [34,31,33,28,30,29,34,30,32,33,32,31];
+au1 = [31,32,33,33,32,34,35,32,30,31,30,27];
 
-new Chart(document.getElementById("line-chart"), {
+uk2 = [24,35,54,57,46,36,35,38,40,44,42,46];
+us2 = [42,44,43,46,43,41,38,36,38,38,36,35];
+ca2 = [30,32,33,31,32,34,30,32,35,37,39,35];
+au2 = [31,32,33,33,32,34,35,32,30,31,30,27];
+
+uk = [];
+us = [];
+ca = [];
+au = [];
+
+uk = uk0;
+us = us0;
+ca = ca0;
+au = au0;
+
+function set(id) {
+    if (id=0) {
+        uk = uk0;
+        us = us0;
+        ca = ca0;
+        au = au0;
+    }
+    if (id=1) {
+        uk = uk1;
+        us = us1;
+        ca = ca1;
+        au = au1;
+    }
+    else {
+        uk = uk2;
+        us = us2;
+        ca = ca2;
+        au = au2;
+    }
+    chart.update();
+}
+
+var chart = new Chart(document.getElementById("line-chart"), {
     type: 'line',
     data: {
-        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        labels: dates,
         datasets: [{
-            data: [86,114,106,106,107,111,133,221,783,2478],
-            label: "Africa",
+            data: uk,
+            label: "UK",
             borderColor: "#3e95cd",
             fill: false
         }, {
-            data: [282,350,411,502,635,809,947,1402,3700,5267],
-            label: "Asia",
+            data: us,
+            label: "USA",
             borderColor: "#8e5ea2",
             fill: false
         }, {
-            data: [168,170,178,190,203,276,408,547,675,734],
-            label: "Europe",
+            data: ca,
+            label: "Canada",
             borderColor: "#3cba9f",
             fill: false
         }, {
-            data: [40,20,10,16,24,38,74,167,508,784],
-            label: "Latin America",
+            data: au,
+            label: "Australia",
             borderColor: "#e8c3b9",
-            fill: false
-        }, {
-            data: [6,3,2,2,7,26,82,172,312,433],
-            label: "North America",
-            borderColor: "#c45850",
             fill: false
         }
         ]
@@ -67,7 +107,8 @@ new Chart(document.getElementById("line-chart"), {
     options: {
         title: {
             display: true,
-            text: 'World population per region (in millions)'
+            text: 'Popularity of Songs by Country over the last year'
         }
     }
-})
+});
+
